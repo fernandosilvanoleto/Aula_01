c34cb82d6fc8b24e51cbfaa9637cac13fda8145a
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-04-02 21:04:13 -0300

    Ajustes de parâmetros.

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) =>{
+/**
+ * ROTA / RECURSO
+ */
+
+ /**
+  * MÉTODOS HTTP
+  * 
+  * GET: Buscar/listar uma informações do back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Alterar uma informação no back-end
+  * DELETE: Deletar uma informaçãoo do back-end
+  */
+
+  /**
+   * Tipos de parâmetors
+   * 
+   * Query Params: Parâmetros nomeados enviados na rota após "?" (servem para filtro, paginação)
+   * Route Params: Parâmetros utilizados para identificar recursos "/:Id"
+   * Request Params: Corpo da requisição, utilizado para criar ou alterar recurso
+   */
+
+app.get('/users', (request, response) => {
     return response.json({
         evento: "Semana Teste 0.0.0",
         pessoa: "Fernando Silva Noleto"
@@ -10,5 +31,4 @@ app.get('/', (request, response) =>{
 });
 
 app.listen(3333); //acessar através do ip do navegador
-
 
