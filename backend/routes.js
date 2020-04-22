5bb85000af0112872780e6f1c7c347b741ab3960
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-04-22 18:34:46 -0300

    Inserindo primeiros dados de pessoas.

--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,29 @@
 const express = require('express');
-
+const crypto = require('crypto');
+const connection = require('./src/database/connection');
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
+routes.get('/ongs', async (request, response) =>{
+    const ongs = await connection('ongs').select('*');
+    
+    return response.json(ongs);
+});
+
+routes.post('/ongs', async (request, response) => {
+    const data = request.body;
+
+    console.log(data);
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        'name':"fernando",
+        'email':"fernando@unitins.br",
+        'whatsapp':"fernando_web",
+        'city':"Miracema",
+        'uf': "TO"
+    })
+
     return response.json({
         evento: "Semana Teste 0.0.1",
         pessoa: "Fernando Silva Noleto"
--- backend/src/database/migrations/20200421171619_create_ongs.js
+++ backend/src/database/migrations/20200421171619_create_ongs.js
@@ -2,11 +2,11 @@
 exports.up = function(knex) {
    return knex.schema.createTable('ongs', function(table){
         table.string('id').primary();
-        table.string('name').notNullabe();
-        table.string('email').notNullabe();
-        table.string('whatsapp').notNullabe();
-        table.string('city').notNullabe();
-        table.string('uf', 2).notNullabe();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
    });
 };
 
