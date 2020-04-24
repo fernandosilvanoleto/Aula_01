7b1d46be8424e00b973ff5176dc3ab7366bdd2de
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-04-24 16:22:00 -0300

    Implementando rotas para listagam específica de incidentes e login de usuário.

--- backend/routes.js
+++ backend/routes.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const OngController = require('./src/controllers/OngController');
 const IncidentController = require('./src/controllers/IncidentController');
+const ProfileController = require('./src/controllers/ProfileController');
+const Sessionontroller = require('./src/controllers/SessionController');
 const routes = express.Router();
+
+routes.post('/sessions', Sessionontroller.create);
 
 routes.get('/ongs', OngController.index);
 
 routes.post('/ongs', OngController.create);
 
+routes.get('/profile', ProfileController.index);
+
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
+routes.delete('incidents/:id', IncidentController.delete);
 
 // exportando rotas, faz isso para outro arquivo conseguir ver
 module.exports = routes;
--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -12,11 +12,29 @@ module.exports = {
         const ong_id = request.headers.authorization;
 
        const [id] = await connection('incidents').insert({
-            'title':"Caso 1",
-            'description':"Caso teste 1",
+            'title':"Caso 2 - Fernando",
+            'description':"Caso teste 2 - Ceulp/Ulbra",
             'value':100,
             'ong_id': "5c824d71",
         });
         return response.json({ id });
+    },
+
+    async delete(request, response){
+        const { id } = response.params;
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+        
+        if (incident.ong_id != ong_id){
+            return response.status(401).json({error: 'Operation not permitted'});
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
     }
 };
