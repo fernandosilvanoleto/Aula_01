56023afc8168930df8456fcbb8ad87148b2fee6b
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-04-26 11:54:27 -0300

    Implementando Componentes, JSX e propriedades do React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
 
+// quando o código em html está integrado dentro do js é chamado de JSX
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header>
+      Bem vindo!!! Fernando Silva Noleto
+    </Header>
   );
 }
 
