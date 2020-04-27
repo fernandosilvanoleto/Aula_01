2160f028f6b60a64a8cdbcee48f63310ba78d3dd
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-04-27 20:07:23 -0300

    Implementando conceitos de estados e estilização de login.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState } from 'react';
+
+import './global.css';
+
+import Logon from './pages/Logon';
 
 // quando o código em html está integrado dentro do js é chamado de JSX
 function App() {
+  /*
+  // conceito de estado
+  const [counter, setCounter] = useState(0);
+
+  // usestate retorna duas arrays -> Array [valor, atualização]
+
+  
+  function Increment(){
+    setCounter(counter + 1);
+    //console.log(counter);
+  }
+  // usando dentro do return
+  <div>
+      <Header>
+        Contador: {counter}
+      </Header>
+      <!-- <button onClick= {Increment}>Incrementar</button> ->
+    </div>
+  */
+
   return (
-    <Header>
-      Bem vindo!!! Fernando Silva Noleto
-    </Header>
+    <Logon />
   );
 }
 
