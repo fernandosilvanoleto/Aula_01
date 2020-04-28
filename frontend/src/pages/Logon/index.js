64d0b95d2f0c417068b4d8b4ec4f937fe14fa698
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-04-28 18:19:19 -0300

    Implementando tela de login.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
+import heroesImg from '../../assets/Hero.jpg';
+import logoImg from '../../assets/Logo.png';
+
 export default function Logon(){
     return (
-        <h1>Hello World!!!</h1>
+        <div className="logon-controller">
+             <section className="form">
+             <img src={heroesImg} alt="Logo"/>
+             <form>
+                 <h1>Faça Login</h1>
+
+                 <input placeholder="Seu ID" />
+                 <button className="button" type="submit">Entrar</button>
+
+                 <a href="/registrar">
+                     <FiLogIn size={16} color="#E02041" />
+                      Não tenho cadastro
+                 </a>
+             </form>
+             </section>
+
+             <img src={logoImg} alt="Heroes"/>
+        </div>
     );
 }
