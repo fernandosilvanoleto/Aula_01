9d26df26227c31127bb279b37d9900e0d59a8e9b
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2021-06-12 09:51:26 -0300

    Removendo Node js e alterações de backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//RENDER -> COLOCAR EM TELA!!!
+//RENDER -> COLOCAR EM TELA aqui!!!
 ReactDOM.render(<App />, document.getElementById('root'));
 
